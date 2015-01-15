52754a1a413360d56bbe4b0a4ca605cf288f1db1
Monika Fischer <monikafisch@gmail.com>
2015-01-15 07:47:33 -0800

    Plotting total normalized luciferase data
    ggplot2 and base plot

--- LuciferaseStats.R
+++ LuciferaseStats.R
@@ -83,9 +83,6 @@ dat.dt["WT9"] #after setting the key, there's no need to use "strain==" to pull 
 setkey(dat.dt, strain, date) #re-set the key to sort by two columns instead of one.
 dat.dt[list("WT9", "6.Feb.14")] #outputs row with "WT9" in the strain column and "the date"6.Feb.14" in the date column
 
-
-datc <- dcast(melt(datnovars), variable~strain, id.vars=c("date", "strain"))
-
 #
 # plotting with ggplot2 ####
 theme_opts <- list(theme(panel.background = element_rect(fill="white"),
@@ -102,7 +99,9 @@ theme_opts <- list(theme(panel.background = element_rect(fill="white"),
                           #axis.title.x = element_blank(),
                           #axis.title.y = element_blank(),
                           #plot.title = element_text(size=22)))
+                          #xlab(expression(paste(" ", Delta, "gene"))) #add Greek Delta to title!
                   
+library("ggplot2", lib.loc="/Library/Frameworks/R.framework/Versions/3.0/Resources/library")
 ggplot(datm.mean.se)+
   geom_bar(aes(variable, V1), fill="orange", color="red", stat="identity")+
   geom_errorbar(aes(x=variable, ymax=V2, ymin=V3), color="purple", width=0.3)+
@@ -116,33 +115,148 @@ gap.barplot(datm.mean.se$V1, gap=c(400,2700))
 #Reshape data directly from the luminometer: ####
 #each row is a strain, each collumn is a rep #######
 library("reshape2", lib.loc="/Library/Frameworks/R.framework/Versions/3.0/Resources/library")
+rm(dat)
 dat <- read.table(file.choose(), header=TRUE)
 head(dat)
-#rotate table so that each collumn is a strain and each row is a rep:
-datcast <- dcast(melt(dat), variable~strain)
+datcast <- dcast(melt(dat), variable~strain) #rotate table so collumns=strains and rows=reps
 head(datcast)
-#after using dcast the first row is the vars, remove it:
-dat2 <- datcast[-1,]
+dat2 <- datcast[-1,] #after using dcast the first row is the vars, remove it
 head(dat2)
 #dcast also renamed the first collumn to "variable", which gets confusing later with melt,
-#so I changed the name of the first collumn back to "vars":
+#so I changed the name of the first collumn back to "reps":
 colnames(dat2)[1] <- "reps"
-datm <- melt(dat2, id.vars="reps")
-#reps is now a column that functions as the vars
+datm <- melt(dat2, id.vars="reps") #reps is now a column that functions as the vars
 head(datm)
 anova <- aov(value~variable, data=datm)
 summary(anova)
-TukeyHSD(anova)
 tukey <- TukeyHSD(anova)
-head(tukey)
 result <- data.frame(tukey$variable)
 head(result)
 result["p.adj"]
 #something about printing only the p values changes the way the
-#p-values are formated, for example 0.0000146 --> 1.457372e-05
+#p-values are formated, for example 0.0000146 --> 1.457372e-05 ...but not always...
+plot(TukeyHSD(anova, "variable")) #plots TukeyHSD p-values...
+
+###
+### ANOVAs on dataframe containing all of my raw luciferase data
+
+totlumdat <- read.table(file.choose(), header=TRUE)
+datcast <- dcast(melt(totlumdat[totlumdat$date=="12.Jan.15",]), variable~strain)
+dat2 <- datcast[-1,]
+colnames(dat2)[1] <- "reps"
+datm <- melt(dat2, id.vars="reps") #reps is now a column that functions as the vars
+datmo <- na.omit(datm) #remove any row that contains an NA
+anova <- aov(value~variable, data=datmo)
+summary(anova)
+tukey <- TukeyHSD(anova)
+result <- data.frame(tukey$variable)
+result["p.adj"]
+#
+datmoNoWT <- datmo[-c(71:80), ]
+datmoNoWT
+anova <- aov(value~variable, data=datmoNoWT)
+summary(anova)
+tukey <- TukeyHSD(anova)
+result <- data.frame(tukey$variable)
+result["p.adj"]
+#
+datmoWTonly <- datmo[-c(1:66), ]
+datmoWTonly
+t.test(value~variable, data=datmoWTonly)
+
+#plotting normalized data ####
+rm(dat)
+dat <- read.table(file.choose(), header=TRUE)
+head(dat)
+plot(normavg~group, data=dat, ylab="Normalized Averages", xlab=".")
+
+library("ggplot2", lib.loc="/Library/Frameworks/R.framework/Versions/3.0/Resources/library")
+datnovars <- dat[ ,-1] #remove first column, which is "vars"
+head(datnovars)
+dat.dt <- data.table(dat)
+setkey(dat.dt, group)
+ggplot(dat.dt)+
+  geom_point(aes(organize,normavg))
+  
+#### alex had to play with this -------
+alex.dat<-dat.dt
+#create ordered factor
+alex.dat[ ,group_f:=factor(group, levels=c('low', 'med', 'high'))]
+alex.dat
+#create plot
+alex.p<-ggplot(alex.dat)+
+  geom_point(aes(strain, normavg, color=group_f))+
+  scale_x_discrete(limits=alex.dat[order(group_f), unique(strain)])
+alex.p
+#
+alex.dat<-dat.dt
+#create ordered factor
+alex.dat[ ,group_f:=factor(group, levels=c('low', 'med', 'high'))]
+alex.dat
+#create plot
+alex.p<-ggplot(alex.dat)+
+  geom_point(aes(strain, normavg, color=group_f))+
+  scale_x_discrete(limits=alex.dat[order(group_f), unique(strain)])
+alex.p
+
+#saturday morning mess around
+head(alex.dat)
+alex.dat[ ,lapply(.SD, class)]
+summary(alex.dat)
+alex.dat[ ,rep12:=NULL]
+
+alex.melt<-melt(alex.dat, id.vars=c("date", "strain", "gene", "isolate", "group", "group_f"), na.rm=TRUE)
+alex.melt
+ggplot(alex.melt)+
+  geom_point(aes(gene, value, color=group_f))+
+  scale_x_discrete(limits=alex.melt[order(group_f), unique(gene)])+
+  theme(axis.text.x = element_text(angle=90))
+##
+normdat <- read.table(file.choose(), header=TRUE)
+head(normdat)
+normdat.dt <- data.table(normdat)
+setkey(normdat.dt, date)
+normdat.dt.noaug21 <- normdat.dt[!"21.Aug.14"]
+normdat.dt.noaug21may18 <- normdat.dt.noaug21[!"18.May.14"] 
+#remove all rows containing 21.Aug.14 because this experiment lacked mak2 as a control.
+#normdat.dt[ ,rep12:=NULL] #remove column 12 because it's all NA's ...removed in Excel
+normdat.dt.noaug21may18[ ,group_f:=factor(group, levels=c('low', 'med', 'high'))]
+plot(normavg~group_f, data=normdat.dt.noaug21may18, 
+     ylab="Normalized Average Luciferase Output", xlab="Group")
+normdat.dt.m <-melt(normdat.dt.noaug21may18, id.vars=c("date", "strain", "gene", "isolate", "group", "group_f"), na.rm=TRUE)
+normdat.dt.m
+ggplot(normdat.dt.m)+
+  geom_point(aes(gene, value, color=group_f))+
+  scale_x_discrete(limits=normdat.dt.m[order(group_f), unique(gene)])+
+  #scale_x_discrete(limits=normdat.dt.m[order(gene), unique(gene)])+ #alphabetical by gene
+  theme(axis.text.x = element_text(angle=90, color="black"))+
+  theme(legend.title=element_blank())+
+  theme(panel.background = element_rect(fill="white"))+
+  theme(panel.grid.minor = element_blank())+
+  theme(panel.grid.major = element_line(color="grey90"))+  #grey100 = white, grey0 = black
+  theme(axis.ticks = element_line(color="grey90"))+
+  theme(axis.text.y = element_text(color="black"))+
+  ylab("Normalized Luceriferase Output")+
+  xlab(expression("Gene"))
+  #scale_x_discrete(limits=normdat.dt.m[order(c("mak2", "nrc1", "ham5", "bem1", "pp1", "ada3", "adv1", 
+                            "so", "ham4", "ham8", "ham9", "ham7", "ham6", "nox1", 
+                            "ham11", "ham14", "mob3", "spr7", "ptp2", "vib1", 
+                            "nik2", "cse1", "lao1", "arg15", "ham12", "WT")), unique(gene)]) 
+
+
+#import raw data?
+install.packages("XML")
+library(XML)
+dat.raw<-htmlTreeParse("/Users/monikafischer/Dropbox/GlassLab/Luciferase/Raw_Data_Files/2Dec2013.mht")
+dat.raw<-readLines("/Users/monikafischer/Dropbox/GlassLab/Luciferase/Raw_Data_Files/2Dec2013.mht")
+dat.raw[grep("</TABLE>", dat.raw)]
+#never mind, kinda complicated, check if can export in different format...
+
+#by the way, I can use the path instead of file.choose() to import data,
+
+
 
 # t.tests on a dataframe made by mannually entering in values with c() and rbind ##########
-
 #This code gives a different answer and I'm not sure why....
 #WT <- c(4520, 4600, 2720, 3760, 3320, 5320, 4000, 3240, 9600, 12160, 11640)
 #ham11 <- c(680, 600, 560, 440, 560, 640, 840, 440, 1880, 2200, 1800)
